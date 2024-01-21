a54b19145ee9ee3b2a9a6838329e2b8b7cc67007
ZachNich <raiste.doctow@gmail.com>
2024-01-20 21:22:30 -0600

    create demoTable

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
-use axum::{
-    routing::get,
-    Router,
-};
+// use axum::{
+//     routing::get,
+//     Router,
+// };
+
+use sqlx::Connection;
+use sqlx::Row;
 
 #[tokio::main]
-async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+async fn main() -> Result<(), sqlx::Error> {
+    // let app = Router::new().route("/", get(|| async { "Hello, World!" }));
 
-    axum::Server::bind(&"0.0.0.0:1337".parse().unwrap())
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+    // axum::Server::bind(&"0.0.0.0:1337".parse().unwrap())
+    //     .serve(app.into_make_service())
+    //     .await
+    //     .unwrap();
+
+    // postgresql://[YOUR_USERNAME]:[YOUR_PASSWORD]@[YOUR_HOST_NAME]:[YOUR_PORT]/[DATABASE_NAME]
+    let uri = "postgresql://postgres:postgres@localhost:5432/thingdom";
+    let mut conn = sqlx::postgres::PgConnection::connect(uri).await?;
+    sqlx::query("CREATE TABLE demoTable ( demoCol int )").execute(&mut conn).await?;
+
+    Ok(())
 }
